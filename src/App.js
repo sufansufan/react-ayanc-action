3af53364bf3b377933adc6cc8577b8bb0c90a403
sufansufan <31531763+sufansufan@users.noreply.github.com>
2018-11-13 15:02:43 +0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { Button } from "antd";
+import "./app.css";
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Button type="primary">Primary</Button>
+        <header className="App-header" />
+      </div>
+    );
+  }
+}
+
+export default App;
